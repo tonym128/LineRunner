b55e4c8d2e7a9ac15ea4a3f692bc30a04a40817d
Tony Mamacos <tmamacos@gmail.com>
2020-06-09 23:29:14 +0200

    It's alive

--- game.cpp
+++ game.cpp
@@ -1,7 +1,16 @@
-void gameSetup() {
+#include "game.h"
 
+void gameSetup()
+{
+    cross_setup();
+    cross_loop_end();
 }
 
-void gameLoop() {
+void gameLoop()
+{
+    if (!cross_loop_start())
+        return;
 
+    cross_loop_end();
+    cross_clear_screen();
 }
--- game.h
+++ game.h
@@ -1,2 +1,14 @@
+#ifdef ARDUINO
+#include "cross_arduino.h"
+#else
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+typedef unsigned char byte;
+#include "cross_sdl.h"
+#endif
+
+#include "fixpoint.h"
+
 void gameSetup();
 void gameLoop();
